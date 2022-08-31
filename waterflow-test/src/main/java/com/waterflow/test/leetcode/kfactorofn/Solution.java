3e4a9369adf4e561689e7164146212765a1dc378
chenlisong <chenlisong@Koolearn-inc.com>
2022-08-31 11:55:46 +0800

    寻找n的第k个因子 -陈礼松

---
+++ waterflow-test/src/main/java/com/waterflow/test/leetcode/kfactorofn/Solution.java
@@ -0,0 +1,25 @@
+package com.waterflow.test.leetcode.kfactorofn;
+
+public class Solution {
+
+    public int kthFactor(int n, int k) {
+
+        int cnt = 0;
+        for(int factor=1; factor<=n; factor++) {
+            if(n % factor == 0) {
+                ++cnt;
+            }
+
+            if(k == cnt) {
+                return factor;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().kthFactor(12, 3));
+    }
+
+}
