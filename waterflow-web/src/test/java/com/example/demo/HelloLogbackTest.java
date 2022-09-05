e5761217237b0c45985f3526e32714bff5c7e758
chenlisong <chenlisong@Koolearn-inc.com>
2022-09-05 17:38:21 +0800

    junit load dev config.

--- waterflow-web/src/test/java/com/example/demo/HelloLogbackTest.java
+++ waterflow-web/src/test/java/com/example/demo/HelloLogbackTest.java
@@ -1,21 +1,26 @@
 package com.example.demo;
 
 import com.waterflow.service.HelloService;
+import com.waterflow.web.init.Application;
 import org.junit.Test;
+import org.junit.internal.Classes;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(classes= Application.class)
+@RunWith(SpringRunner.class)
+@ActiveProfiles("dev")
 public class HelloLogbackTest {
 
 	private Logger logger = LoggerFactory.getLogger(HelloLogbackTest.class);
 
 	@Test
 	public void helloTest(){
-
 		String words = "xx";
-
 		System.out.println("words out print is: " + words);
 		logger.info("words log print is: " + words);
 	}
