fd2b0da9d03129bae57423ba462ba744ad41679f
chenlisong <chenlisong@Koolearn-inc.com>
2022-09-13 20:13:39 +0800

    remove invalid code

--- waterflow-web/src/main/java/com/waterflow/web/init/Application.java
+++ waterflow-web/src/main/java/com/waterflow/web/init/Application.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
